8869c96213146f6c9c06f4129f8d6d97ac098825
skrzy <st.krzysiek6@gmail.com>
2016-05-08 12:40:52 +0200

    moduł modelu

---
+++ ejb/remote/src/main/java/pl/polsl/reservations/user/UserFacadeRemote.java
@@ -0,0 +1,10 @@
+package pl.polsl.reservations.user;
+
+import javax.ejb.Remote;
+
+/**
+ * Created by Krzysztof Stręk on 2016-05-07.
+ */
+@Remote
+public interface UserFacadeRemote {
+}
---
+++ ejb/user/src/main/java/pl/polsl/reservations/user/UserFacade.java
@@ -0,0 +1,11 @@
+package pl.polsl.reservations.user;
+
+import javax.ejb.Stateless;
+
+/**
+ * Created by Krzysztof Stręk on 2016-05-07.
+ */
+@Stateless
+public class UserFacade implements UserFacadeRemote {
+
+}
