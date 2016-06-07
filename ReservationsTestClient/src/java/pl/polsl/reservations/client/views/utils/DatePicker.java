0cf64dfb3caceb34d4ac5677da8b11a19eb8d30f
Andrzej997 <matisojka666@interia.pl>
2016-06-07 18:29:16 +0200

    DatePicker

--- ReservationsTestClient/src/java/pl/polsl/reservations/client/views/utils/DatePicker.java
+++ ReservationsTestClient/src/java/pl/polsl/reservations/client/views/utils/DatePicker.java
@@ -1,12 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pl.polsl.reservations.client.views.utils;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Component;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Properties;
 import javax.swing.JFormattedTextField;
 import javax.swing.JPanel;
@@ -21,57 +17,78 @@ import org.jdatepicker.impl.UtilDateModel;
  */
 public class DatePicker extends JPanel {
 
-    private static DatePicker weekDatepicker;
-    private static DatePicker dayDatepicker;
-    private final JDatePanelImpl datePanel;
-    private final JDatePickerImpl datePicker;
-    private final UtilDateModel model;
+    private static final DatePicker weekDatepicker = new DatePicker(true);
+    private static final DatePicker dayDatepicker = new DatePicker(false);
+    private JDatePanelImpl datePanel;
+    private JDatePickerImpl datePicker;
+    private UtilDateModel model;
 
-    public DatePicker(boolean day) {
+    private DatePicker(boolean day) {
         model = new UtilDateModel();
         Properties p = new Properties();
         p.put("text.today", "Today");
         p.put("text.month", "Month");
         p.put("text.year", "Year");
         datePanel = new JDatePanelImpl(model, p);
-       if(!day)
-        {
-        datePicker = new JDatePickerImpl(datePanel, new WeekDateFormatter());
-        PanelStyle.setSize(datePanel, 220, 200);
-        add(datePicker);
-        weekDatepicker = this;
+        if (!day) {
+            datePicker = new JDatePickerImpl(datePanel, new WeekDateFormatter());
+            PanelStyle.setSize(datePanel, 220, 200);
+            add(datePicker);
+        } else {
+            datePicker = new JDatePickerImpl(datePanel, new DayDateFormatter());
+            PanelStyle.setSize(datePanel, 220, 200);
+            PanelStyle.setSize(datePicker, 150, 30);
+            add(datePicker);
         }
-       else {
-        datePicker = new JDatePickerImpl(datePanel, new DayDateFormatter());
-        PanelStyle.setSize(datePanel, 220, 200);
-        PanelStyle.setSize(datePicker, 150,30);
-        add(datePicker);
-        dayDatepicker = this; 
-       }
-        
+
     }
 
     public static DatePicker getInstance() {
         return weekDatepicker;
     }
-    
-     public static DatePicker getDayInstance() {
+
+    public static DatePicker getDayInstance() {
         return dayDatepicker;
     }
-    
 
     public DateModel<?> getModel() {
         return datePicker.getModel();
     }
 
+    public void setDate(Calendar date, boolean day) {
+        if (!day) {
+            model = new UtilDateModel(getDateFromCalendar(date));
+            Properties p = new Properties();
+            p.put("text.today", "Today");
+            p.put("text.month", "Month");
+            p.put("text.year", "Year");
+            datePanel = new JDatePanelImpl(model, p);
+            datePicker = new JDatePickerImpl(datePanel, new WeekDateFormatter());
+            PanelStyle.setSize(datePanel, 220, 200);
+        } else {
+            model = new UtilDateModel(getDateFromCalendar(date));
+            Properties p = new Properties();
+            p.put("text.today", "Today");
+            p.put("text.month", "Month");
+            p.put("text.year", "Year");
+            datePanel = new JDatePanelImpl(model, p);
+            datePicker = new JDatePickerImpl(datePanel, new DayDateFormatter());
+            PanelStyle.setSize(datePanel, 220, 200);
+            PanelStyle.setSize(datePicker, 150, 30);
+        }
+    }
+
+    public static Date getDateFromCalendar(Calendar date) {
+        Date result = new Date(date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DATE));
+        return result;
+    }
+
     public JFormattedTextField getJFormattedTextField() {
         return datePicker.getJFormattedTextField();
     }
-    
-    public JDatePickerImpl getDatePicker(){
+
+    public JDatePickerImpl getDatePicker() {
         return this.datePicker;
     }
-
-    
 
 }
